c5ecdddbfa9be4ed309afbf026b32275babfe509
Yogesh815131 <ky815131@gmail.com>
2023-12-05 14:12:48 +0530

    String Method

--- StringMethod.js
+++ StringMethod.js
@@ -16,6 +16,39 @@ console.log(str.substr(7, 6)); //first parm start position string and last parm 
 // The replace() method returns a new string.
 // The replace() method replaces only the first match
 // If you want to replace all matches, use a regular expression with the /g flag set. See examples below.
-let text = "Mumbai is a great city, i like mumbai city, Mumbai is big city "
-let str1 = str.replace("Yogesh", "Ajay");
-console.log(str1);
+let text1 = "Mumbai is a great city, i like mumbai city, Mumbai is big city "
+let text2 = text1.replace("Mumbai", "Pune");//replace only first
+console.log(text2);
+let text3 = text1.replace(/Mumbai/g, "Pune");//follow case sensitive
+console.log(text3);
+let text4 = text1.replace(/mumbai/ig, "Pune");//replace all not follow case sensitive
+console.log(text4);
+
+//ReplaceAll() method
+console.log(text1.replaceAll("mumbai", "Nashik"));//follow case sensitive
+console.log(text1.replaceAll(/mumbai/ig, "Nashik"));//not follow case sensitive
+
+//toUpperCase() and toLowerCase() method
+console.log(text1.toUpperCase());
+console.log(text1.toLowerCase());
+
+//concat() method
+let fName = "Yogesh";
+let lName = "Jadhav";
+let fullName = fName.concat(" ", lName);
+console.log("Full Name",fullName);
+
+// trim() method
+let strTrim = "       hello world      ";
+console.log(strTrim.trim());
+
+//trimStart() method
+console.log(strTrim.trimStart());
+//trimEnd() method
+console.log(strTrim.trimEnd());
+
+// split() method
+let bDate = "12/01/2001";
+let year = bDate.split("/");
+console.log(year);
+console.log(year[2]);
